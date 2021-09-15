53d66ed1ef53e3ac79e627cbedf194986c1b5433
Ben Pearce <b.j.pearce@durham.ac.uk>
2021-09-16 05:32:16 +0800

    Added reviews & some styling

--- seeds/index.js
+++ seeds/index.js
@@ -1,36 +1,40 @@
-const mongoose = require("mongoose");
-const Campground = require("../models/campground");
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
-const cities = require("./cities");
-const { places, descriptors } = require("./seedHelpers");
-
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-	console.log("Database connected");
+    console.log("Database connected");
 });
 
-const sample = (array) => array[Math.floor(Math.random() * array.length)];
+const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedData = async function () {
-	await Campground.deleteMany({});
-	for (let i = 0; i < 50; i++) {
-		const randomInt = Math.floor(Math.random() * 1000);
-		const camp = new Campground({
-			location: `${cities[randomInt].city}, ${cities[randomInt].state}`,
-			title: `${sample(descriptors)} ${sample(places)}`,
-		});
-		await camp.save();
-	}
-	console.log("Database has been seeded");
-};
 
-seedData().then(() => {
-	db.close();
-});
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
