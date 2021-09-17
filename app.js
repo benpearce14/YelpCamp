3e752b9f5b30e515193fd50abcdd75a9b8a8c874
Ben Pearce <b.j.pearce@durham.ac.uk>
2021-09-18 00:55:48 +0800

    Added some security fixes

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const favicon = require("serve-favicon");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
+const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
-const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
+const helmet = require("helmet");
+const favicon = require("serve-favicon");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
@@ -42,24 +44,74 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(
+	mongoSanitize({
+		replaceWith: "_",
+	})
+);
 
 const sessionConfig = {
-	secret: "thisshouldbeabettersecret",
+	name: "session",
+	secret: "thisshouldbeabettersecret!",
 	resave: false,
 	saveUninitialized: true,
 	cookie: {
+		httpOnly: true,
+		// secure: true,
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7,
-		HttpOnly: true,
 	},
 };
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+	"https://stackpath.bootstrapcdn.com",
+	"https://api.tiles.mapbox.com",
+	"https://api.mapbox.com",
+	"https://kit.fontawesome.com",
+	"https://cdnjs.cloudflare.com",
+	"https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+	"https://kit-free.fontawesome.com",
+	"https://stackpath.bootstrapcdn.com",
+	"https://api.mapbox.com",
+	"https://api.tiles.mapbox.com",
+	"https://fonts.googleapis.com",
+	"https://use.fontawesome.com",
+];
+const connectSrcUrls = [
+	"https://api.mapbox.com",
+	"https://*.tiles.mapbox.com",
+	"https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", "blob:"],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				"blob:",
+				"data:",
+				"https://res.cloudinary.com/dsc8tgwww/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				"https://images.unsplash.com/",
+			],
+			fontSrc: ["'self'", ...fontSrcUrls],
+		},
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
